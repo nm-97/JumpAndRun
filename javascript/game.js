2e4ca044830ac8b59555449ba8e21009e80d2b24
nm-97 <your-email@example.com>
2025-08-26 16:34:52 +0200

    changes in assets

--- javascript/game.js
+++ javascript/game.js
@@ -3,5 +3,5 @@ let char = new Image();
 
 function startLandingPage() {
   canvas = document.getElementById("gameCanvas");
-  char.src = "../";
+  char.src = "../assets/char/lvl_1/shadow/idle/idle-0.png";
 }
